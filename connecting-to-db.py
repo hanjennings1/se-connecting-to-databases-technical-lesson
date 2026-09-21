3683ccd6c1992e14f7e5cb8e1ed6e9278405fb07
Hannah Jennings <han.jennings1@gmail.com>
2026-09-21 09:55:28 -0400

    practice with sql complete

--- connecting-to-db.py
+++ connecting-to-db.py
@@ -1,2 +1,15 @@
 import sqlite3
 import pandas as pd
+
+conn = sqlite3.connect('data.sqlite') # path to connect to database
+cur = conn.cursor() # open
+
+df = pd.DataFrame(
+    cur.execute("""SELECT * FROM offices;""").fetchall(),
+    columns=[x[0] for x in cur.description]
+)
+
+print(df) #printing out result
+
+conn.close() # close
+
